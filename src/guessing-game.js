8919edafab30eb5c4adda190d3008b28e2ab1b0e
Sasha Ivanova <sashaivanova.pro@gmail.com>
2024-08-28 21:57:40 +0500

    feat:add wrong decision for wron tests

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,17 +1,11 @@
 class GuessingGame {
-  constructor() {
-    this.min = 0;
-    this.max = 0;
-    this.candidate = 0;
-  }
-
   setRange(min, max) {
     this.min = min;
     this.max = max;
   }
 
   guess() {
-    this.candidate = Math.floor((this.min + this.max) / 2);
+    this.candidate = Math.round((this.min + this.max) / 2);
     return this.candidate;
   }
 
