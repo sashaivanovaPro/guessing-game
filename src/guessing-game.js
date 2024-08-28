cfd7d2d9eea7eebdf003394eca388c1eda32c9d1
Sasha Ivanova <sashaivanova.pro@gmail.com>
2024-08-28 15:07:47 +0600

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.candidate = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.candidate = Math.floor((this.min + this.max) / 2);
+    return this.candidate;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.candidate - 1;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.candidate + 1;
+  }
 }
 
 module.exports = GuessingGame;
